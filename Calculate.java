8180e805eee6c9ed9c4c8cc20fd5da6fad9e8d81
SashaShvets <slippersasha@ukr.net>
2016-05-22 14:33:32 +0300

    update Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,23 @@
+import java.lang.Math;
 public class Calculate{
   
-   public static void main(String[] arg){
+   public static void main(String[] args){
 
-   System.out.println("Calculate...");
+     System.out.println("Calculate...");
+   int first = Integer.valueOf(args[0]);
+   int second = Integer.valueOf(args[1]);
+   int summ = first + second;
+   int minus = first - second;
+   int multipl = first * second;
+   int divis = first / second;  
+   int sqr = (int) Math.pow(first, second);
+
+   
+   System.out.println("Сумма "+summ);
+   System.out.println("Разница "+minus);
+   System.out.println("Умножение "+multipl);
+   System.out.println("Деление "+divis);
+   System.out.println("Возведение в степень "+sqr);
+    
   }
 }
